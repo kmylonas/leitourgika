ba9b2e0af2a2c92c516a1feae4c8a8a5f541a1d5
kmylonas <kostakismil@gmail.com>
2020-11-11 00:33:56 +0200

    TOGA EISAI KADOS

--- kernel_threads.c
+++ kernel_threads.c
@@ -37,8 +37,8 @@ Tid_t sys_CreateThread(Task task, int argl, void* args)
   @brief Return the Tid of the current thread.
  */
 Tid_t sys_ThreadSelf()
-{
-  Tid_t tid = (Tid_t)(cur_thread()->ptcb);
+{ TCB* tcb = cur_thread();
+  Tid_t tid = (Tid_t)tcb->ptcb;
 	return tid;
 }
 
@@ -47,23 +47,26 @@ Tid_t sys_ThreadSelf()
   */
 int sys_ThreadJoin(Tid_t tid, int* exitval)
 {
+  // if(tid=!ThreadSelf())
+  //   printf("Im different"); 
   PCB* curproc = CURPROC;
   PTCB *ptcb = (PTCB*)tid;
   rlnode* node = rlist_find(&curproc->ptcb_list, ptcb, NULL);//ptcb_list_node
   //Tid_t node_tid = (Tid_t)(node->obj);
-  if(node == NULL){
-    printf("No thread with tid %u found\n", tid);
+  if(node == NULL || node->ptcb->detached==1 || node->ptcb->exited==1 || tid == sys_ThreadSelf()){//
+    printf("HeheError joining the thread with ID %u\n", tid);
     return -1;
   }
+  node->ptcb->refcount++;
   while(node->ptcb->exited==0 && node->ptcb->detached==0){//T2 not exited and T2 no detached
-    kernel_wait(node->ptcb->exit_cv, SCHED_USER);//T1 sleeps, it will return here when we broadcast it(in thread exit)
+    kernel_wait(&node->ptcb->exit_cv, SCHED_USER);//T1 sleeps, it will return here when we broadcast it(in thread exit)
   }
-  exitval=node->ptcb->exitval;
+  node->ptcb->refcount--;
+  exitval=&node->ptcb->exitval;
+  if(node->ptcb->refcount==0)
+    free(node->ptcb);
+	return 0;
   
-
-
-
-	return -1;
 }
 
 /**
@@ -71,15 +74,20 @@ int sys_ThreadJoin(Tid_t tid, int* exitval)
   */
 int sys_ThreadDetach(Tid_t tid)
 {
+  if(tid == NOTHREAD)
+    return -1;
   PCB* curproc = CURPROC;
   PTCB *ptcb = (PTCB*)tid;
   rlnode* node = rlist_find(&curproc->ptcb_list, ptcb, NULL);//node=ptcb_list_node
-  Tid_t node_tid = (Tid_t)node->obj;
-  if(node->ptcb->detached==1 || node==NULL || node->ptcb->exited==1)
+  if(node==NULL)
+    return -1;
+  Tid_t node_tid = (Tid_t)node->ptcb;
+  if(node->ptcb->detached==1 || node->ptcb->exited==1)
 	  return -1;
   else
   {
     node->ptcb->detached = 1;
+    kernel_broadcast(&node->ptcb->exit_cv);
     return 0;
   }
   
@@ -90,7 +98,65 @@ int sys_ThreadDetach(Tid_t tid)
   */
 void sys_ThreadExit(int exitval)
 {
-  T2->exited=1
-broadcast!!
+
+  PCB* curproc = CURPROC;
+  TCB* curthread = cur_thread();
+ //COPY FROM EXIT
+ if(curproc->thread_count==1){
+    /* Reparent any children of the exiting process to the 
+       initial task */
+    PCB* initpcb = get_pcb(1);
+    while(!is_rlist_empty(& curproc->children_list)) {
+      rlnode* child = rlist_pop_front(& curproc->children_list);
+      child->pcb->parent = initpcb;
+      rlist_push_front(& initpcb->children_list, child);
+    }
+
+    /* Add exited children to the initial task's exited list 
+       and signal the initial task */
+    if(!is_rlist_empty(& curproc->exited_list)) {
+      rlist_append(& initpcb->exited_list, &curproc->exited_list);
+      kernel_broadcast(& initpcb->child_exit);
+    }
+
+    /* Put me into my parent's exited list */
+    rlist_push_front(& curproc->parent->exited_list, &curproc->exited_node);
+    kernel_broadcast(& curproc->parent->child_exit);
+
+  
+
+  assert(is_rlist_empty(& curproc->children_list));
+  assert(is_rlist_empty(& curproc->exited_list));
+d list */
+  /* Release the args data */
+  if(curproc->args) {
+    free(curproc->args);
+    curproc->args = NULL;
+  }
+
+  /* Clean up FIDT */
+  for(int i=0;i<MAX_FILEID;i++) {
+    if(curproc->FIDT[i] != NULL) {
+      FCB_decref(curproc->FIDT[i]);
+      curproc->FIDT[i] = NULL;
+    }
+  }
+
+  /* Disconnect my main_thread */
+  curproc->main_thread = NULL;
+
+  /* Now, mark the process as exited. */
+  curproc->pstate = ZOMBIE;
+
+
+ }//END COPY EXIT
+ if(curthread->ptcb->refcount==0){
+   free(curthread->ptcb);
+ }
+curthread->ptcb->exitval = exitval;
+curproc->thread_count--;
+kernel_broadcast(&curthread->ptcb->exit_cv);
+  /* Bye-bye cruel world */
+kernel_sleep(EXITED, SCHED_USER);
 }
 
